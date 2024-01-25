fbbd8095a1e31cdb55c290bcf21331550b286de7
Arnold Supriyadi <arnoldsupriyadi@gmail.com>
2024-01-25 23:07:55 +0700

    video 39 define policy

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -2,26 +2,25 @@
 
 namespace App\Policies;
 
-use App\Models\Permission;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
-class PermissionPolicy
+class UserPolicy
 {
     /**
      * Determine whether the user can view any models.
      */
     public function viewAny(User $user): bool
     {
-        return $user->hasPermission('permission_access');
+        //
     }
 
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Permission $permission): bool
+    public function view(User $user, User $model): bool
     {
-        return $user->hasPermission('permission_view');
+        //
     }
 
     /**
@@ -29,23 +28,38 @@ class PermissionPolicy
      */
     public function create(User $user): bool
     {
-        return $user->hasPermission('permission_create');
+        //
     }
 
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, Permission $permission): bool
+    public function update(User $user, User $model): bool
     {
-        return $user->hasPermission('permission_update'); 
+        //
     }
 
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Permission $permission): bool
+    public function delete(User $user, User $model): bool
     {
-        return $user->hasPermission('permission_delete');
+        //
     }
 
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, User $model): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, User $model): bool
+    {
+        //
+    }
 }
